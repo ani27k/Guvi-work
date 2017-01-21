9fa86774101d1b3f6acb5299ea1047094b2333fd
ani27k <anikanungo@gmail.com>
2017-01-21 18:25:24 +0530

    Update num.java

--- num.java
+++ num.java
@@ -1,12 +1,5 @@
-import java.util.Scanner;
-class num
-{
-	public static void main(String[] args)
-	{
-		Scanner s = new Scanner(System.in);
-		int a = s.nextInt();
-		if(a==0)System.out.println("zero");
-		if(a>0)System.out.println("postive");
-		if(a<0)System.out.println("negative");
-	}
-}
+Scanner s = new Scanner(System.in);
+int a = s.nextInt();
+if(a==0)System.out.println("zero");
+if(a>0)System.out.println("postive");
+if(a<0)System.out.println("negative");
